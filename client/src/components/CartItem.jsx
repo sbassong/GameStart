58deaa4ab8d2b7009b55641d596708d67b5ff6f0
Sam Bassong <sam.bassong@gmail.com>
2021-09-20 12:33:25 -0700

    set up cartItem component

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
-const CartItem = () => {
+const CartItem = (props) => {
+  const decrementQuantity = () => {}
+  const incrementQuantity = () => {}
 
   return (
-
+    <div className='item-card'>
+      <section>
+        <img src={props.background_image} alt="" />
+        <p>{props.name}</p>
+      </section>
+      
+      <section>
+        <div><p>{props.price}</p></div>
+        <button onClick={decrementQuantity} >-</button>
+        <input type="text" />
+        <button onClick={incrementQuantity} >+</button>
+      </section>
+    </div>
   )
 }
 
