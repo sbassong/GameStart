5d51a491ab9bd755ee7a5139f04de83aeaa62deb
Sam Bassong <sam.bassong@gmail.com>
2021-09-20 10:17:16 -0700

    set up some services for Users and Reviews. set up api services

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './styles/App.css';
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <main>
+        <Switch>
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+        </Switch>
+      </main>
     </div>
   );
 }
