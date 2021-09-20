6fb4007a82e2a7f461e82e28bf022974ad87efb7
Sam Bassong <sam.bassong@gmail.com>
2021-09-20 12:24:41 -0700

    set up base gameCard component

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
-const GameCard = () => {
+const GameCard = (props) => {
 
   return (
+    <div className='game-card'>
+      <section><img src={props.background_image} alt="" /></section>
+      <section className='hover-info'>
+        <p>{props.name}</p>
+        <p>{props.price}</p>
+      </section>
+    </div>
 
   )
 }
