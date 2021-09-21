38832b8559809e141ee57b52da5d19125fde4680
Sam Bassong <sam.bassong@gmail.com>
2021-09-20 17:03:22 -0700

    set up all userServices

--- client/src/services/UserServices.js
+++ client/src/services/UserServices.js
@@ -2,7 +2,7 @@ import Client from './api'
 
 export const SignInUser = async (data) => {
   try {
-    const res = await Client.post('/auth/login', data)
+    const res = await Client.post('/login', data)
     localStorage.setItem('token', res.data.token)
     return res.data.user
   } catch (error) {
@@ -12,7 +12,7 @@ export const SignInUser = async (data) => {
 
 export const SignUpUser = async (data) => {
   try {
-    const res = await Client.post('/auth/signup', data)
+    const res = await Client.post('/signup', data)
     return res.data
   } catch (error) {
     throw error
@@ -22,17 +22,59 @@ export const SignUpUser = async (data) => {
 export const CheckSession = async () => {
   try {
     // Checks if the current token if it exists is valid
-    const res = await Client.get('/auth/session')
+    const res = await Client.get('/session')
     return res.data
   } catch (error) {
     throw error
   }
 }
 
-export const GetUser = async () => {
+export const GetUser = async (user_id) => {
   try {
-    const res = await Client.get('')
+    const res = await Client.get(`/${user_id}`)
+    console.log(res.data)
+    return res.data
   } catch (error) {
-    
+    throw error
+  }
+}
+
+export const GetAllUsers = async () => {
+  try {
+    const res = await Client.get(`/`)
+    console.log(res.data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const UpdatePassword = async (data) => {
+  try {
+    const res = await Client.post('/update/password', data)
+    console.log(res.data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const UpdateUser = async (data) => {
+  try {
+    const res = await Client.put('/update', data)
+    console.log(res.data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const DeleteUser = async (user_id) => {
+  try {
+    const res = await Client.destroy(`/delete/${user_id}`)
+    console.log(res.data)
+    return res.data
+  } catch (error) {
+    throw error
   }
 }
