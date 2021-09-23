38f99d5b7f48036ffb439e6a84592032b4aabe62
Sam Bassong <sam.bassong@gmail.com>
2021-09-23 10:47:02 -0700

    set up SubmitReviewForm functionality

--- client/src/components/SubmitReviewForm.jsx
+++ client/src/components/SubmitReviewForm.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CreateReview } from '../services/ReviewServices'
 
-const SubmitReviewForm = (props) => {
+const SubmitReviewForm = ({user, game}) => {
+  const [formValues, setFormValues] = useState({user_id: user.id, game_id: game.id, content: ''})
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await CreateReview(formValues)
+    alert('Thank you for posting your review!')
+  }
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
   return (
-    <div>
-      form
+    <div className='review-cont'>
+      <form className='review-form' onSubmit={handleSubmit}>
+        <input type="text" value={formValues.content} onChange={handleChange} name="content" placeholder='Please write review here'/>
+        <button className='post-review'>Post Review</button>
+      </form>
     </div>
   )
 }
-
-
 
 export default SubmitReviewForm
