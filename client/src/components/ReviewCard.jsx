2ededab0482b85757aa6a0883870cf707f1bce06
Sam Bassong <sam.bassong@gmail.com>
2021-09-23 12:11:04 -0700

    set up get reviews on gameDetails page functionality and conditional rendering of SubmitReviewForm

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { UpdateReview, DeleteReview } from '../services/ReviewServices'
+import { GetUser } from '../services/UserServices'
 
-const Review = ({ user, comment }) => {
+const ReviewCard = ({ review }) => {
+  const [reviewerName, setReviewerName] = useState
+
+  const getUserName = async () => {
+    const reviewer = await GetUser(review.user_id)
+    setReviewerName(reviewer.name)
+  }
+
+  useEffect(() => {
+    getUserName()
+  }, [])
   return (
     <div>
-      <h2>{user.name}</h2>
-      <p>{comment.content}</p>
+      <h2>{reviewerName}</h2>
+      <p>{review.content}</p>
+      <section className='hover-review'>
+        
+      </section>
     </div>
   )
 }
 
-export default Review
+export default ReviewCard
--- client/src/pages/GameDetails.js
+++ client/src/pages/GameDetails.js
@@ -1,12 +1,26 @@
 import React, {useEffect, useState} from "react";
 import { AddToCart, GetCart } from "../services/CartServices";
+import { GetReviewsByGameId } from "../services/ReviewServices";
+import ReviewCard from "../components/ReviewCard";
+import SubmitReviewForm from "../components/SubmitReviewForm";
 
 const GameDetails = ({game, user}) => {
   const [cart, setCart] = useState({})
+  const [gameReviews, setGameReviews] = useState([])
+  const [reviewButton, toggleReviewButton] = useState(false)
+
+  const showReviewForm = () => {
+    reviewButton ? toggleReviewButton(false) : toggleReviewButton(true)
+  }
 
   const findCart = async () => {
     const res = await GetCart(user.id)
     setCart(res)
+  }
+
+  const getReviews = async () => {
+    const reviews = await GetReviewsByGameId(game.id)
+    setGameReviews(reviews)
   }
   
   const cart_item = {
@@ -16,19 +30,32 @@ const GameDetails = ({game, user}) => {
 
   useEffect(() => {
     if (user) findCart()
+    getReviews()
   }, [])
 
   return (
     <div>
-      <section><img src={game.background_image} alt="" /></section>
-      <section>
-        <h1>{game.title}</h1>
-        <p>Rating: {game.rating}</p>
-        <p>USD ${game.price}</p>
-        <p>Platform: {game.platform}</p>
-        <p>Description: {game.description}</p>
-        <button className='add-button' onClick={() => { AddToCart(cart_item) }}>Add to Cart</button> 
+      <section className='top-half'>
+        <div><img src={game.background_image} alt="" /></div>
+      
+        <section>
+          <h1>{game.title}</h1>
+          <p>Rating: {game.rating}</p>
+          <p>USD ${game.price}</p>
+          <p>Platform: {game.platform}</p>
+          <p>Description: {game.description}</p>
+          <button className='add-cart' onClick={() => { AddToCart(cart_item) }}>Add to Cart</button>
+          <button className='show-review-form' onClick={showReviewForm}>Post Review</button>
+          {reviewButton && <SubmitReviewForm user={user} game={game}/> }
+        </section>
       </section>
+
+      <section className='reviews-cont'>
+        {gameReviews.map(review => (
+          <ReviewCard key={review.id} review={review} />
+        ))}
+      </section>
+      
     </div>
   )
 }
