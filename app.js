d03fdbddb61c3fde64992c56678ab9ffc44825ac
Sam Bassong <sam.bassong@gmail.com>
2021-09-24 14:48:51 -0700

    pushing to heroku

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const app = require('express')()
+const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
@@ -8,7 +8,7 @@ const path = require('path')
 const AppRouter = require('./routes/AppRouter')
 
 const PORT = process.env.PORT || 3001
-
+const app = express()
 app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
